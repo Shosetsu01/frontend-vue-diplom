8103875f3d837c673ed69abd9c7f0af794383d3c
Марьин Роман Алексеевич <romamarin16@gmail.com>
2022-05-12 10:42:44 +0300

    created main, user and results pages

---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <Header/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header";
+
+export default {
+  name: 'Main',
+  components: {
+    Header
+  }
+}
+</script>
