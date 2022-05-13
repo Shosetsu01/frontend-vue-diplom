7775e34103a1924202f8f8b6ae0c7a7430ba9f58
Polyakov Pavel <polyakovpavel2000@mail.ru>
2022-05-13 12:40:52 +0300

    create Intro

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="results">
     <h1>This is an results page</h1>
+    <Intro></Intro>
   </div>
 </template>
+
+<script>
+import Intro from "@/components/Intro";
+export default {
+  components: {
+    Intro,
+  }
+}
+</script>
